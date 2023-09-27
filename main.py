28ef8491ab2245358d58b99f00bdc1719b76994c
Pola Borkowska <poolaborkowska@gmail.com>
2023-09-27 20:18:59 +0200

    m

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from frame4 import Frame4
 
 class Window(tk.Tk):
     '''class of a custom window that inherits from Tkinter's Tk class'''
-    
+
     def __init__(self):
         super().__init__()
-        
-        #setting parameters of the window (title, size, colors)
+
+        # setting parameters of the window (title, size, colors)
         self.title('Wander Wisely')
-        self.photo = tk.PhotoImage(file = "map.png")
+        self.photo = tk.PhotoImage(file="images\\map.png")
         self.iconphoto(False, self.photo)
         self.geometry("918x700")
         self.minsize(918, 700)
@@ -24,18 +24,18 @@ class Window(tk.Tk):
         self.highlight = '#1c3c4f'
         self.details = '#162f3d'
         self.configure(background=self.bgColor)
-        
-        #shared variables between frames
+
+        # shared variables between frames
         self.countryName = tk.StringVar(value='your country')
         self.baseCurrency = tk.StringVar(value='your base currency')
         self.codeCurrency = tk.StringVar(value='codeCurrency')
-        
-        #call the start method to initialize the frames
+
+        # call the start method to initialize the frames
         self.start()
 
     def start(self):
-        #create instances of custom frame classes and pass necessary parameters
-        
+        # create instances of custom frame classes and pass necessary parameters
+
         self.frame1 = Frame1(self, self.bgColor, self.details, self.highlight,
                              self.countryName, self.baseCurrency, self.codeCurrency)
         self.frame2 = Frame2(self, self.bgColor, self.details, self.highlight,
@@ -44,25 +44,24 @@ class Window(tk.Tk):
                              self.frame1, self.countryName, self.baseCurrency)
         self.frame4 = Frame4(self, self.bgColor, self.details, self.highlight,
                              self.frame1, self.countryName, self.baseCurrency)
-        
-        #set references to the frames within frame1
+
+        # set references to the frames within frame1
         self.frame1.setFrames(self.frame1, self.frame2,
                               self.frame3, self.frame4)
 
-        #place frames on the grid
+        # place frames on the grid
         for frame in (self.frame1, self.frame2, self.frame3, self.frame4):
             frame.grid(row=0, column=0, sticky='nsew')
 
-        #initially display only frame1 and hide the rest
+        # initially display only frame1 and hide the rest
         self.frame1.grid(row=0, column=0, sticky='nsew')
         self.frame2.grid_remove()
         self.frame3.grid_remove()
         self.frame4.grid_remove()
 
 
-
 if __name__ == '__main__':
     windll.shcore.SetProcessDpiAwareness(1)
-    
+
     app = Window()
     app.mainloop()
